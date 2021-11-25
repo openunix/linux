eb4b489e5bbd5bd5dc051b8fd1066999507964ec
Jeffle Xu <jefflexu@linux.alibaba.com>
2021-11-25 15:05:28 +0800

    fuse: negotiate per inode DAX in FUSE_INIT
    Among the FUSE_INIT phase, client shall advertise per inode DAX if it's
    mounted with "dax=inode". Then server is aware that client is in per
    inode DAX mode, and will construct per-inode DAX attribute accordingly.
    
    Server shall also advertise support for per inode DAX. If server doesn't
    support it while client is mounted with "dax=inode", client will
    silently fallback to "dax=never" since "dax=inode" is advisory only.
    
    Signed-off-by: Jeffle Xu <jefflexu@linux.alibaba.com>
    Reviewed-by: Vivek Goyal <vgoyal@redhat.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 2ee019fadcca343c3deea6a1767965bdf23fc3d0)

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -786,6 +786,9 @@ struct fuse_conn {
 	/* Add supplementary group info when creating a new inode */
 	unsigned int create_supp_group:1;
 
+	/* Does the filesystem support per inode DAX? */
+	unsigned int inode_dax:1;
+
 	/* Is tmpfile not implemented by fs? */
 	unsigned int no_tmpfile:1;
 
