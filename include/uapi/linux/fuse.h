a464dc807247a9322be3966c4f81161c0a30c3fc
Jingbo Xu <jefflexu@linux.alibaba.com>
2024-02-26 11:54:35 +0800

    fuse: add support for explicit export disabling
    open_by_handle_at(2) can fail with -ESTALE with a valid handle returned
    by a previous name_to_handle_at(2) for evicted fuse inodes, which is
    especially common when entry_valid_timeout is 0, e.g. when the fuse
    daemon is in "cache=none" mode.
    
    The time sequence is like:
    
    	name_to_handle_at(2)	# succeed
    	evict fuse inode
    	open_by_handle_at(2)	# fail
    
    The root cause is that, with 0 entry_valid_timeout, the dput() called in
    name_to_handle_at(2) will trigger iput -> evict(), which will send
    FUSE_FORGET to the daemon.  The following open_by_handle_at(2) will send
    a new FUSE_LOOKUP request upon inode cache miss since the previous inode
    eviction.  Then the fuse daemon may fail the FUSE_LOOKUP request with
    -ENOENT as the cached metadata of the requested inode has already been
    cleaned up during the previous FUSE_FORGET.  The returned -ENOENT is
    treated as -ESTALE when open_by_handle_at(2) returns.
    
    This confuses the application somehow, as open_by_handle_at(2) fails
    when the previous name_to_handle_at(2) succeeds.  The returned errno is
    also confusing as the requested file is not deleted and already there.
    It is reasonable to fail name_to_handle_at(2) early in this case, after
    which the application can fallback to open(2) to access files.
    
    Since this issue typically appears when entry_valid_timeout is 0 which
    is configured by the fuse daemon, the fuse daemon is the right person to
    explicitly disable the export when required.
    
    Also considering FUSE_EXPORT_SUPPORT actually indicates the support for
    lookups of "." and "..", and there are existing fuse daemons supporting
    export without FUSE_EXPORT_SUPPORT set, for compatibility, we add a new
    INIT flag for such purpose.
    
    Reviewed-by: Amir Goldstein <amir73il@gmail.com>
    Signed-off-by: Jingbo Xu <jefflexu@linux.alibaba.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit e022f6a1c711ab6d76e9e59dce77e2b25df75076)
    (cherry picked from commit 52da885067f8e85cd263f991f1e79ef84e6d7504)

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -211,6 +211,12 @@
  *  7.39
  *  - add FUSE_DIRECT_IO_ALLOW_MMAP
  *  - add FUSE_STATX and related structures
+*
+  *  7.40
+ *  - add max_stack_depth to fuse_init_out, add FUSE_PASSTHROUGH init flag
+ *  - add backing_id to fuse_open_out, add FOPEN_PASSTHROUGH open flag
+ *  - add FUSE_NO_EXPORT_SUPPORT init flag
+ *
  *  7.42
  *  - Add FUSE_OVER_IO_URING and all other io-uring related flags and data
  *    structures:
@@ -419,6 +425,7 @@ struct fuse_file_lock {
  *			symlink and mknod (single group that matches parent)
  * FUSE_HAS_EXPIRE_ONLY: kernel supports expiry-only entry invalidation
  * FUSE_DIRECT_IO_ALLOW_MMAP: allow shared mmap in FOPEN_DIRECT_IO mode.
+ * FUSE_NO_EXPORT_SUPPORT: explicitly disable export support
  * FUSE_OVER_IO_URING: Indicate that client supports io-uring
  */
 #define FUSE_ASYNC_READ		(1 << 0)
@@ -459,6 +466,7 @@ struct fuse_file_lock {
 #define FUSE_CREATE_SUPP_GROUP	(1ULL << 34)
 #define FUSE_HAS_EXPIRE_ONLY	(1ULL << 35)
 #define FUSE_DIRECT_IO_ALLOW_MMAP (1ULL << 36)
+#define FUSE_NO_EXPORT_SUPPORT	(1ULL << 38)
 
 /* Obsolete alias for FUSE_DIRECT_IO_ALLOW_MMAP */
 #define FUSE_DIRECT_IO_RELAX	FUSE_DIRECT_IO_ALLOW_MMAP
