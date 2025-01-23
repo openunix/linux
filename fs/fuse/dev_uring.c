be8b53fc2252f127391838f964319ab742c35d10
Bernd Schubert <bschubert@ddn.com>
2025-01-23 17:55:32 +0100

    fuse: prevent disabling io-uring on active connections
    The enable_uring module parameter allows administrators to enable/disable
    io-uring support for FUSE at runtime. However, disabling io-uring while
    connections already have it enabled can lead to an inconsistent state.
    
    Fix this by keeping io-uring enabled on connections that were already using
    it, even if the module parameter is later disabled. This ensures active
    FUSE mounts continue to function correctly.
    
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    Reviewed-by: Luis Henriques <luis@igalia.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 2d4fde59fd502a65c1698b61ad4d0f10a9ab665a)
    (cherry picked from commit ac738c15aad070446c3739b979113848803a173f)

--- fs/fuse/dev_uring.c
+++ fs/fuse/dev_uring.c
@@ -1105,11 +1105,6 @@ int fuse_uring_cmd(struct io_uring_cmd *cmd, unsigned int issue_flags)
 	u32 cmd_op = cmd->cmd_op;
 	int err;
 
-	if (!enable_uring) {
-		pr_info_ratelimited("fuse-io-uring is disabled\n");
-		return -EOPNOTSUPP;
-	}
-
 	if ((unlikely(issue_flags & IO_URING_F_CANCEL))) {
 		fuse_uring_cancel(cmd, issue_flags);
 		return 0;
@@ -1125,6 +1120,12 @@ int fuse_uring_cmd(struct io_uring_cmd *cmd, unsigned int issue_flags)
 		return -ENOTCONN;
 	}
 	fc = fud->fc;
+
+	/* Once a connection has io-uring enabled on it, it can't be disabled */
+	if (!enable_uring && !fc->io_uring) {
+		pr_info_ratelimited("fuse-io-uring is disabled\n");
+		return -EOPNOTSUPP;
+	}
 
 	if (fc->aborted)
 		return -ECONNABORTED;
