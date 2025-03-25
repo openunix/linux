e4aff1a4c64e0d3bded577925d21554d590025be
Bernd Schubert <bschubert@ddn.com>
2025-03-25 18:29:31 +0100

    fuse: {io-uring} Fix a possible req cancellation race
    task-A (application) might be in request_wait_answer and
    try to remove the request when it has FR_PENDING set.
    
    task-B (a fuse-server io-uring task) might handle this
    request with FUSE_IO_URING_CMD_COMMIT_AND_FETCH, when
    fetching the next request and accessed the req from
    the pending list in fuse_uring_ent_assign_req().
    That code path was not protected by fiq->lock and so
    might race with task-A.
    
    For scaling reasons we better don't use fiq->lock, but
    add a handler to remove canceled requests from the queue.
    
    This also removes usage of fiq->lock from
    fuse_uring_add_req_to_ring_ent() altogether, as it was
    there just to protect against this race and incomplete.
    
    Also added is a comment why FR_PENDING is not cleared.
    
    Fixes: c090c8abae4b ("fuse: Add io-uring sqe commit and fetch support")
    Cc: <stable@vger.kernel.org> # v6.14
    Reported-by: Joanne Koong <joannelkoong@gmail.com>
    Closes: https://lore.kernel.org/all/CAJnrk1ZgHNb78dz-yfNTpxmW7wtT88A=m-zF0ZoLXKLUHRjNTw@mail.gmail.com/
    Signed-off-by: Bernd Schubert <bschubert@ddn.com>
    Reviewed-by: Joanne Koong <joannelkoong@gmail.com>
    Signed-off-by: Miklos Szeredi <mszeredi@redhat.com>
    (cherry picked from commit 09098e62e4be8f0755e58d6078aaf27cbd9a3a8d)
    (cherry picked from commit bbe104140373b1b2a53a11c9e7facf115fc26ac9)

--- fs/fuse/fuse_i.h
+++ fs/fuse/fuse_i.h
@@ -339,6 +339,7 @@ struct fuse_io_priv {
  * FR_FINISHED:		request is finished
  * FR_PRIVATE:		request is on private list
  * FR_ASYNC:		request is asynchronous
+ * FR_URING:		request is handled through fuse-io-uring
  */
 enum fuse_req_flag {
 	FR_ISREPLY,
@@ -353,6 +354,7 @@ enum fuse_req_flag {
 	FR_FINISHED,
 	FR_PRIVATE,
 	FR_ASYNC,
+	FR_URING,
 };
 
 /**
@@ -402,6 +404,7 @@ struct fuse_req {
 
 #ifdef CONFIG_FUSE_IO_URING
 	void *ring_entry;
+	void *ring_queue;
 #endif
 };
 
